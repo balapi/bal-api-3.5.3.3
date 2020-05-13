5f909f7b1e69d0ce100b9f95fddbcec70b0e50a1
BAL API <balapi@broadcom.com>
2020-05-13 12:05:09 -0700

    BAL API

---
+++ doc/html/navtreedata.js
@@ -0,0 +1,46 @@
+var NAVTREE =
+[
+  [ "BCM686xx Management API", "index.html", [
+    [ "BCM686xx API Concept", "index.html", [
+      [ "Introduction", "index.html#intro", null ],
+      [ "System Model", "index.html#object_model", null ],
+      [ "Object and Properties Implementation", "index.html#object_model_prop", [
+        [ "Object Structures", "index.html#object_model_structs", null ]
+      ] ]
+    ] ],
+    [ "Object Model", "object_model.html", "object_model" ],
+    [ "BCM686xx API Reference", "modules.html", "modules" ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"group__api.html",
+"group__object__model.html#ga4a1d349c3fb42d2aaea9e07d966085d4",
+"group__object__model.html#gac3766213397667d2101cf62b3d3e6a22",
+"group__object__model.html#gga0b52715b615776502a0e72ef58d26828a42211b0937a5437e88faa9c98c210fb7",
+"group__object__model.html#gga23d6467b8ec8e1d02987931637eaa188af87ed9a2bc14c5777d361bacac63fa0f",
+"group__object__model.html#gga3d757bf68b982b5ddf06f3f2629addf2aafa72ac6688998edf89e505c6d376b17",
+"group__object__model.html#gga57295c9ce6b249fc382952e2cdddde01ab06e6b340891dfc9c966156d2c3db967",
+"group__object__model.html#gga70292ba8b2bfd67db892d3c175d07cbdaf7e263826ddf5ef6ab4a4a7224e7270f",
+"group__object__model.html#gga915126d53b9a1f52c942578bed1c5d2eae50ffc6e4ea832f92550786c4c3e3b44",
+"group__object__model.html#ggaa96eca84a0955824830c2718d6b1bb73a6646f694ce319de9601e4bb4f72ddd2a",
+"group__object__model.html#ggab368d91553c7e51a2cc12b46b75c7b4ba6a08c7bd7ee1230f5c2c8ca4a603db24",
+"group__object__model.html#ggacb3d1fc1648624f815e5e0b1db649d2da2f90eef8e733fc6290c34e77a5b694bc",
+"group__object__model.html#ggae58aebe8347dfdf1ca375c4fb2daeaf4a75309ffa0c15330f36f7c40966a71e8c",
+"group__object__model.html#ggaf1a32f39c3c7e2b8385ccdd1a6bb2014ad2c1f76846000bacade24ad288509141",
+"object_itupon_alloc_configuration_completed.html",
+"structbcmolt__arr__u16__2.html#a7099be7adf9ff82b5a23ff8e4802e4e6",
+"structbcmolt__device__image__transfer__start__data.html#a7ba6d986496f16e7c9bb92ea0265914d",
+"structbcmolt__gpio__cfg.html",
+"structbcmolt__itu__pon__params.html#a668bd9ed99ca7b0f26abc2912b191305",
+"structbcmolt__log__file__cfg__data.html#a6a019648a53352a413ea79b958a362cd",
+"structbcmolt__onu__auto__cfg__data.html#a3f3331df4a0026558358297d02de86a2",
+"structbcmolt__onu__onu__enable__completed__data.html#a852c5c56e58b41b2a1eb16ebac530c85",
+"structbcmolt__pbit__to__tc__multi__cfg.html#a63c59ec226863d92f6e0d4467f3b1df7",
+"structbcmolt__pon__interface__protection__switching__switchover__completed__data.html#aa3d444927d87dbc2e70c533bd2670340",
+"structbcmolt__system__profile.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ doc/html/object_erps_interface.js
@@ -0,0 +1,8 @@
+var object_erps_interface =
+[
+    [ "Object configuration", "object_erps_interface_cfg.html", null ],
+    [ "Indication: erps_port_update", "object_erps_interface_erps_port_update.html", null ],
+    [ "Indication: ring_port_control", "object_erps_interface_ring_port_control.html", null ],
+    [ "Operation: port_update", "object_erps_interface_port_update.html", null ],
+    [ "Operation: set_ring_port_control", "object_erps_interface_set_ring_port_control.html", null ]
+];
